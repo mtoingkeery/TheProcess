ee0605e1e18a8665d5d80673edc3e29729945c2d
Zach Hou <zach.houzy@gmail.com>
2018-05-12 00:43:45 +0800

    wechat send text

--- lib/fdmt_wechat.py
+++ lib/fdmt_wechat.py
@@ -6,26 +6,29 @@ Created on Fri May 11 11:24:23 2018
 """
 
 import json,datetime,time,os
+import urllib
 from urllib.request import urlopen
 import pandas as pd
 import fdmt_query as fdq
 
-def access_token(agent_name="Message"):
-    try:
-        config_path=os.environ["THE_PROCESS"].replace("\\","/")+"/config/"
-        wechat_config_file=config_path+"wechat_config.json"
-        wechat_config=json.load(open(wechat_config_file))
-        
-        current_time = datetime.datetime.now()
-        current_time_str=current_time.strftime("%Y-%m-%d %H:%M:%S")
-        
-        corp_id=wechat_config["Main"]["CorpID"]
+
+config_path=os.environ["THE_PROCESS"].replace("\\","/")+"/config/"
+wechat_config_file=config_path+"wechat_config.json"
+wechat_config=json.load(open(wechat_config_file))
+
+current_time = datetime.datetime.now()
+current_time_str=current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+corp_id=wechat_config["Main"]["CorpID"]
+
+def wechat_access_token(agent_name="Message"):
+    try:        
         message_agent_id=wechat_config[agent_name]["AgentID"]
         message_agent_secret=wechat_config[agent_name]["Secret"]
         
         access_token_file=config_path+"wechat_access_token_"+message_agent_id+".json"
         access_token_obj=json.load(open(access_token_file))
-        
+
         if access_token_obj["ExpireTime"]>current_time_str:
             print(time.strftime('%Y/%m/%d %T')+" - Access Token Valid")
             return [0,access_token_obj["AccessToken"]]
@@ -54,4 +57,42 @@ def access_token(agent_name="Message"):
         print(time.strftime('%Y/%m/%d %T')+" - Exception Occurs!")
         print(str(ErrorCode))
         print("-----------------------------------------------------------------------------")
+        return [200,""]
+
+def wechat_send(message_content="Hello World! via Python",agent_name="Message"):
+    para_res=wechat_access_token("Message")    
+    if para_res[0]<=1:
+        access_token=para_res[1]
+        target_url="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+access_token
+
+        contact_user=wechat_config["Contact"]["UserID"]
+        message_agent_id=int(wechat_config[agent_name]["AgentID"])
+
+        headers = {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        }
+
+        post_data={
+           "touser" : contact_user,
+           "msgtype" : "text",
+           "agentid" : message_agent_id,
+           "text" : {
+               "content" : message_content
+           },
+           "safe":0
+           }
+           
+        post_date_enc=json.dumps(post_data).encode('utf-8')
+        post_request = urllib.request.Request(target_url,data=post_date_enc,headers=headers)
+        post_res = eval(urllib.request.urlopen(post_request).read().decode('utf-8'))
+        
+        if post_res["errcode"]==0:
+            print(time.strftime('%Y/%m/%d %T')+" - Send Successfully")
+            return [1,"Send Successfully"]
+        else:
+            print(time.strftime('%Y/%m/%d %T')+" - Send Successfully")
+            return [100,"Failed to Send"]            
+        
+    else:
+        print(time.strftime('%Y/%m/%d %T')+" - No Access Token Available")
         return [200,""]
