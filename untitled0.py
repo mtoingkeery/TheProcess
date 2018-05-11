4a99d0a3a60d3f3579a44b0eaeb5c724b788a80b
marcus <houz@dianrong.com>
2018-05-11 20:02:24 +0800

    regular update

--- untitled0.py
+++ untitled0.py
@@ -5,7 +5,16 @@ Created on Fri May 11 11:24:23 2018
 @author: houz
 """
 
+from urllib.request import urlopen
+import requests,json
 
-import fdmt_data as fdt
+corp_id="wwf972aeec5c872dd3"
+corp_secret="Z5R2GCHnBL1Xa3xl61yW6fgGfGn9E25pBaSYklndIUI"
+#target_url="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+corp_id+"&corpsecret="+corp_secret
+target_url='https://www.baidu.com/'
+print(target_url)
+html = urlopen(target_url).read()
+html=b'{"errcode":0,"errmsg":"ok","access_token":"UwmFuJzUPSBFfXwI__OgvnRI_6JJKFK5XeOLBABF8DTFciht5RALNWWUjoIBRP2DfjoL89pXzjn9yHc1nNRZdIHZMYPBOivrXpcRkcKONAKAm3wO3jyKthdPRpdcM6Rum_iyOmAl7c0k94TExzvvewDVEICmMdoKvVSIYB4W4Uyadno2Jat5bRIrDb81_IunWhsTEEAMVwFJB0N9NZpvQA","expires_in":7200}'
 
-fdt.pgs_update_stk_cov('60','90000',1)
+
+print(eval(html.decode("utf-8")))
