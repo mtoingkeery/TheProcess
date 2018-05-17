520c9d4229f32debf6b52acaedb4ac1569f28c8b
zach.hou <zongyuan.hou@dianrong.com>
2018-05-17 16:49:53 +0800

    system regular upate

--- src/TP099_Generate_Batch.py
+++ src/TP099_Generate_Batch.py
@@ -13,6 +13,8 @@ def main():
     main_path=fdt.main_path
     bin_path=main_path+"bin/"
 
+    ##################################################################################
+    #Bat 01
     exec_query01=''
     for para in os.environ["Path"].split(";"):
         if para.replace("\\","").replace("\/","")[-9:]=="Anaconda3":
@@ -22,13 +24,24 @@ def main():
     exec_query04=main_path+"log/"+"ERROR_LOG.log"
     exec_query00=exec_query01+" "+exec_query02+" >> "+exec_query03+" 2>> "+exec_query04+"\n"
 
-    print(exec_query00)
-
     bat_file=open(bin_path+"the_process.bat",'w+')
 
     bat_file.write("cd/\n")
     bat_file.write("c:\n")
     bat_file.write(exec_query00)
+
+    bat_file.close()
+
+    ##################################################################################
+    #Bat 02
+    bat_file=open(bin_path+"regular_update.bat",'w+')
+
+    bat_file.write("cd/\n")
+    bat_file.write("c:\n")
+    bat_file.write("cd "+main_path+"\n")
+    bat_file.write("gitp\n")
+    bat_file.write('''git commit -m "system regular upate"\n''')
+    bat_file.write("git push")
 
     bat_file.close()
 
