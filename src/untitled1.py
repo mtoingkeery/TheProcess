182190805112bdea22303b8defabc3dbb0be74b7
zach.hou <zongyuan.hou@dianrong.com>
2018-05-26 20:00:12 +0800

    system regular upate

--- src/untitled1.py
+++ src/untitled1.py
@@ -9,40 +9,18 @@ import os,time
 import pandas as pd
 import fdmt_data as fdt
 import fdmt_date as fda
-
-data_path=fdt.data_path
-
-para_date_start=fda.current_date_str
-para_date_end=fda.current_date_str
-
-if fda.current_date_str>=fda.date_add(fda.current_month_str,5):
-    para_date_start=fda.current_month_str
-    para_date_end=fda.date_add(fda.current_month_str,1,"MM")
-else:
-    para_date_start=fda.date_add(fda.current_month_str,-1,"MM")
-    para_date_end=fda.date_add(fda.current_month_str,1,"MM")
-
-para_date_start='2014-01-01'
-para_date_end='2015-01-01'
-
-para_mon_start=fda.date_format(para_date_start,"yyyymm")
-para_mon_end=fda.date_format(para_date_end,"yyyymm")
+import fdmt_wechat as fdw
+import urllib as _urllib
+import json
 
 
-hist_list=[]
-for parent,dirnames,filenames in os.walk(data_path+"idx_hist/"):
-    for filename in filenames:
-        if (filename[-4:].lower()=='.txt')&(filename[-17:-11]>=para_mon_start)&(filename[-17:-11]<=para_mon_end):
-            hist_list.append(parent+filename)
+url = "http://apis.baidu.com/apistore/weatherservice/weather?citypinyin="
 
-for para in hist_list:
-    print(time.strftime("%Y/%m/%d %T")+" - "+para[-17:])
-    para_df1=pd.read_csv(para,dtype={'id':str})
+city= "shanghai"
+#完整api访问接口
+target_url=url+city
 
-    
-#    para_df2=para_df1.rename(columns={"utime":"dtime"})
-#    para_df2["utime"]=fda.current_time_str
+headers={"apikey", "vMUI0wh6BUDLR2WkiP3P0oaZtwNgL5iE"}
 
-
-print(para_df1)
-print("X1199000")
+post_request = _urllib.request.Request(target_url,headers=headers)
+post_res = eval(_urllib.request.urlopen(post_request).read().decode('utf-8'))
