f5e794c248d992284e9b3435f712a2c58eb34529
unknown <zongyuan.hou@BKJK.COM>
2018-11-02 15:24:32 +0800

    system regular upate

--- bin/the_process.bat
+++ bin/the_process.bat
@@ -1,3 +1,3 @@
 cd/
 c:
-D:/Program/Anaconda3/python.exe C:/Users/houz/Branch/theProcess/src/TP999_The_Process_Daily.py >> C:/Users/houz/Branch/theProcess/log/RUNNING_LOG.log 2>> C:/Users/houz/Branch/theProcess/log/ERROR_LOG.log
+D:/Application/Anaconda3/python.exe D:/Branch/theProcess/src/TP999_The_Process_Daily.py >> D:/Branch/theProcess/log/RUNNING_LOG.log 2>> D:/Branch/theProcess/log/ERROR_LOG.log
